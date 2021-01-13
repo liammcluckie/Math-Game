804823310d2412103f9580a06fc78df9386c8bf4
liammcluckie <liammcluckie@hotmail.co.uk>
2021-01-13 16:54:25 +0000

    Function added with alerts for all buttons.

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
+// Wait for the DOM to finish loading before running the game
+//Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") == "submit") {
+                alert("You clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
 function runGame(){
+
+// Generate two random numbers between 1 and 25
+// Math.floor rounds to the whole number
+// Math.random generates random numbers
+
+let num1 = Math.floor(Math.random() * 25) + 1;
+let num2 = Math.floor(Math.random() * 25) + 1;
 
 }
 
@@ -27,5 +52,5 @@ function displaySubtractQuestion() {
 }
 
 function displayMultiplyQuestion() {
-    
+
 }
