abc076d8e05186ae9b9f28a50e6a6bbead44a9b3
liammcluckie <liammcluckie@hotmail.co.uk>
2021-01-13 17:02:27 +0000

    Addition function code added.

--- script.js
+++ script.js
@@ -10,13 +10,15 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert("You clicked submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
-function runGame(){
+function runGame(gameType){
 
 // Generate two random numbers between 1 and 25
 // Math.floor rounds to the whole number
@@ -24,6 +26,13 @@ function runGame(){
 
 let num1 = Math.floor(Math.random() * 25) + 1;
 let num2 = Math.floor(Math.random() * 25) + 1;
+
+if (gameType === "addition") {
+    displayAdditionQuestion(num1, num2);
+} else {
+    alert(`Unknown game type ${gameType}`);
+    throw `Unknown game type ${gameType}, aborting!`; // Print error to console
+}
 
 }
 
@@ -43,7 +52,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 
 }
 
